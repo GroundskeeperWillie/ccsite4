4a4cb9fc5d744c64a2639645e92413041fff8e1e
GroundskeeperWillie <wmacaulay@gmail.com>
2013-02-03 11:22:58 +0530

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Ccsite4::Application.routes.draw do
+  get "static_pages/splash"
+
+  get "static_pages/home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
