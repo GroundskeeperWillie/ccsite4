385da0541d1b0e226d517ff7f4922da91fb8f099
GroundskeeperWillie <portlyowl@gmail.com>
2013-03-06 11:55:06 +1100

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Ccsite4::Application.routes.draw do
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
   root :to => 'static_pages#splash'
   
-  match '/home', :to => 'static_pages#home'
-  match '/signup', :to => 'users#new'
-
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+  
   match '/details', :to => 'static_pages#details'
   match '/registry', :to => 'static_pages#registry'
   match '/contact', :to => 'static_pages#contact'
